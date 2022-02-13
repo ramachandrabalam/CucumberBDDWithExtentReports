56444a8e3b23bb124dc43413a7a76fa8e27b13da
ramachandrabalam <rcbalam@gmail.com>
2022-02-13 12:08:07 +0530

    test

--- CucumberFramework/src/test/java/stepDefinitions/MyaccountpageSteps.java
+++ CucumberFramework/src/test/java/stepDefinitions/MyaccountpageSteps.java
@@ -20,12 +20,6 @@ public class MyaccountpageSteps {
 	private Myaccountpage myAccountpage = new Myaccountpage(Driverfactory.getDriver());
 	private Loginpage loginPage = new Loginpage(Driverfactory.getDriver());
 	
-//	@When("^Enter username \"([^\"]*)\" and password \"([^\"]*)\"$")
-//	public void enter_username_and_password(String userName, String password) throws Throwable {
-//		loginPage.enterUsername(userName);
-//		loginPage.enterPassword(password);
-//		loginPage.clickLoginbtn();
-//	}
 
 	@When("^Menu contains the following items$")
 	public void menu_contains_the_following_items(DataTable myaccountList) throws Throwable {
